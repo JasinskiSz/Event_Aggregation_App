fe2e5915e261221bc80f11d7bf8178c87efa2ab9
Rabioxxx <szg.jasinski@gmail.com>
2022-11-27 22:08:16 +0100

    Initial commit

---
+++ src/main/java/com/sda/eventapp/EventAggregationAppApplication.java
@@ -0,0 +1,13 @@
+package com.sda.eventapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAggregationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventAggregationAppApplication.class, args);
+    }
+
+}
