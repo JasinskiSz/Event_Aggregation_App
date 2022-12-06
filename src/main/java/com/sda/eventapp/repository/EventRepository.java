42e0dcf17e56e42dffd49df80e553791357a692d
Przemek Degorski <degorskiprzemyslaw@gmail.com>
2022-12-06 22:09:33 +0100

    Fixed search bar

--- src/main/java/com/sda/eventapp/repository/EventRepository.java
+++ src/main/java/com/sda/eventapp/repository/EventRepository.java
@@ -1,6 +1,7 @@
 package com.sda.eventapp.repository;
 
 import com.sda.eventapp.model.Event;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,5 +9,6 @@ import java.util.List;
 
 public interface EventRepository extends CrudRepository<Event, Long> {
 
+    @Query("SELECT event from Event event where event.title like %?1%")
     List<Event> findAllByTitle(String title);
 }
--- src/main/java/com/sda/eventapp/web/EventController.java
+++ src/main/java/com/sda/eventapp/web/EventController.java
@@ -5,6 +5,7 @@ import com.sda.eventapp.service.EventRepositoryService;
 import com.sda.eventapp.web.dto.EventWithBasicData;
 import com.sda.eventapp.web.mapper.EventMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,19 +21,26 @@ public class EventController {
     private final EventRepositoryService eventRepositoryService;
 
     @GetMapping
-    public String getAllEvents(ModelMap map){
+    public String getAllEvents(ModelMap map, @Param("title") String title){
 
         List<EventWithBasicData> events = EventMapper.toWebpage(eventRepositoryService.findAll());
-        map.addAttribute("events", events);
+
+        map.addAttribute("title", title);
+        if(title != null){
+            map.addAttribute("events", EventMapper.toWebpage(eventRepositoryService.findAllByTitle(title)));
+        }
+        else {
+            map.addAttribute("events", events);
+        }
         return "homepage";
     }
 
     //get mapping
     //return redirect /search>
-    @GetMapping("/search")
+    /*@GetMapping("/search")
     public String getAllEventsByTitle(ModelMap map, @RequestParam String title){
         List<EventWithBasicData> eventsByTitle = EventMapper.toWebpage(eventRepositoryService.findAllByTitle(title));
         map.addAttribute("eventsByTitle", eventsByTitle);
         return "homepage";
-    }
+    }*/
 }
