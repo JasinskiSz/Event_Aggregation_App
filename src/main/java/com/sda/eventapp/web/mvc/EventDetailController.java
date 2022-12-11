839fbd802fbcfc673d17e453e65071dbbe4d4948
Przemek Degorski <degorskiprzemyslaw@gmail.com>
2022-12-11 20:41:48 +0100

    Created basic comment form

--- src/main/java/com/sda/eventapp/web/mvc/EventDetailController.java
+++ src/main/java/com/sda/eventapp/web/mvc/EventDetailController.java
@@ -2,12 +2,11 @@ package com.sda.eventapp.web.mvc;
 
 import com.sda.eventapp.service.EventService;
 import com.sda.eventapp.web.mapper.EventMapper;
+import com.sda.eventapp.web.mvc.form.CreateCommentForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/detail-view")
@@ -19,6 +18,18 @@ public class EventDetailController {
     @GetMapping("/{id}")
     public String getDetailEventView(ModelMap map, @PathVariable("id") Long id){
         map.addAttribute("event", EventMapper.toWebpage(eventService.findById(id)));
+        map.addAttribute("comment", new CreateCommentForm());
         return "event-detail-view";
     }
+
+
+    @PostMapping("/{id}")
+    public String addComment(@ModelAttribute CreateCommentForm commentForm,  @PathVariable("id") Long id){
+
+        return "redirect:/detail-view/" + id;
+    }
+
+
+
+
 }
---
+++ src/main/java/com/sda/eventapp/web/mvc/form/CreateCommentForm.java
@@ -0,0 +1,11 @@
+package com.sda.eventapp.web.mvc.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateCommentForm {
+
+    private String text;
+}
