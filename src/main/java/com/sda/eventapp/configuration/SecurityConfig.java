3f4c3a856c1e82d34b0c29671880abd008032dea
Rabioxxx <szg.jasinski@gmail.com>
2022-12-13 21:08:41 +0100

    Added @Bean to make security work.

--- src/main/java/com/sda/eventapp/configuration/SecurityConfig.java
+++ src/main/java/com/sda/eventapp/configuration/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.sda.eventapp.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
