153cd96aa5788e3c2e5b5c5d362b418a7292d6ea
Szymon Jasiński <33954943+rabioxxx@users.noreply.github.com>
2022-12-19 18:45:11 +0100

    Setting up Spring Security and PasswordEncoder
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>
    
    Preparing User.java for UserDetails
    
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/main/java/com/sda/eventapp/configuration/SecurityConfig.java
+++ src/main/java/com/sda/eventapp/configuration/SecurityConfig.java
@@ -1,21 +1,41 @@
 package com.sda.eventapp.configuration;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                .and()
-                .logout()
-                .and()
-                .build();
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/", "/home", "/user/register")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/sda/eventapp/model/User.java
+++ src/main/java/com/sda/eventapp/model/User.java
@@ -2,7 +2,11 @@ package com.sda.eventapp.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,7 +16,7 @@ import java.util.Set;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -38,4 +42,29 @@ public class User {
                     @JoinColumn(name = "event_id", referencedColumnName = "id",
                             nullable = false)})
     private Set<Event> attendingEvents = new HashSet<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new HashSet<>(Set.of(new SimpleGrantedAuthority("role-user")));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
