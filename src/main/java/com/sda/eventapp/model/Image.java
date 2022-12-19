b3129f847d4b7cf442d7858a99d895e75c6b10be
Przemek Degorski <degorskiprzemyslaw@gmail.com>
2022-12-19 21:19:42 +0100

    Add Image classes

---
+++ src/main/java/com/sda/eventapp/model/Image.java
@@ -0,0 +1,29 @@
+package com.sda.eventapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*@Lob
+    private byte[] content;*/
+
+    private String fileName;
+
+    private String path;
+
+    @OneToOne(mappedBy = "image")
+    private Event event;
+
+
+}
---
+++ src/main/java/com/sda/eventapp/service/ImageService.java
@@ -0,0 +1,14 @@
+package com.sda.eventapp.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ImageService {
+
+
+}
