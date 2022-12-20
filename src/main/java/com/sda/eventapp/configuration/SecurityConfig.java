f24d64cbacfebe8a5bdd0d6fe78bf4b4c6d90529
Adriane0291 <adrianbloch01@gmail.com>
2022-12-20 19:50:01 +0100

    - reverted security

--- src/main/java/com/sda/eventapp/configuration/SecurityConfig.java
+++ src/main/java/com/sda/eventapp/configuration/SecurityConfig.java
@@ -1,21 +1,23 @@
-//package com.sda.eventapp.configuration;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig {
-//    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf().disable()
-//                .authorizeHttpRequests()
-//                .anyRequest().permitAll()
-//                .and()
-//                .formLogin()
-//                .and()
-//                .logout()
-//                .and()
-//                .build();
-//    }
-//}
+package com.sda.eventapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .logout()
+                .and()
+                .build();
+    }
+}
