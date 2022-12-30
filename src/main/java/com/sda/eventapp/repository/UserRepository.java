c0dfb34109d37fa0b44628c39b910aec57cf962f
Adriane0291 <adrianbloch01@gmail.com>
2022-12-30 16:17:37 +0100

    - added search query to search for users

--- src/main/java/com/sda/eventapp/repository/UserRepository.java
+++ src/main/java/com/sda/eventapp/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.sda.eventapp.repository;
 
 import com.sda.eventapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,4 +14,10 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     boolean existsByUsername(String userName);
+
+    @Query(value = "SELECT u.*  FROM user as u " +
+            "INNER JOIN users_events us ON u.id = us.user_id " +
+            "INNER JOIN event e ON us.event_id = e.id " +
+            "WHERE e.id = ?1", nativeQuery = true)
+    List<User> findAllUserInEvent(@Param("id") Long id);
 }
