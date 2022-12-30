5fb27fad32e8dd760890eea5128cc072b817c564
Adriane0291 <adrianbloch01@gmail.com>
2022-12-30 16:16:39 +0100

    - added attribute to @Getmapping with users

--- src/main/java/com/sda/eventapp/web/mvc/controller/EventDetailController.java
+++ src/main/java/com/sda/eventapp/web/mvc/controller/EventDetailController.java
@@ -1,6 +1,7 @@
 package com.sda.eventapp.web.mvc.controller;
 
 import com.sda.eventapp.service.EventService;
+import com.sda.eventapp.service.UserService;
 import com.sda.eventapp.web.mvc.form.CreateCommentForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,20 +15,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class EventDetailController {
     private final EventService eventService;
+    private final UserService userService;
 
     @GetMapping("/{id}")
-    public String getDetailEventView(ModelMap map, @PathVariable("id") Long id){
+    public String getDetailEventView(ModelMap map, @PathVariable("id") Long id) {
         map.addAttribute("event", eventService.findEventViewById(id));
         map.addAttribute("comment", new CreateCommentForm());
         map.addAttribute("comments", eventService.findCommentViewsByEventId(id));
+        map.addAttribute("users", userService.eventUserList(id));
+
         return "event-detail-view";
     }
+
 
     @PostMapping("/{id}")
     public String addComment(@ModelAttribute("comment") @Valid CreateCommentForm form, Errors errors, @PathVariable("id") Long eventID) {
 
         //todo trello reminder #002
-        if(errors.hasErrors()){
+        if (errors.hasErrors()) {
             return "redirect:/detail-view/" + eventID;
         }
         eventService.saveComment(form, eventID);
