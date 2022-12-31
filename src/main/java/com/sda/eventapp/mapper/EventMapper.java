03886d8cec34854111c92d83fd60f67be8030249
Przemek Degorski <degorskiprzemyslaw@gmail.com>
2022-12-31 11:51:19 +0100

    Added Set with User nicknames into toEventView method in EventMapper

--- src/main/java/com/sda/eventapp/mapper/EventMapper.java
+++ src/main/java/com/sda/eventapp/mapper/EventMapper.java
@@ -2,10 +2,12 @@ package com.sda.eventapp.mapper;
 
 import com.sda.eventapp.dto.EventView;
 import com.sda.eventapp.model.Event;
+import com.sda.eventapp.model.User;
 import com.sda.eventapp.web.mvc.form.CreateEventForm;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class EventMapper {
@@ -30,6 +32,7 @@ public class EventMapper {
                 .startingDateTime(event.getStartingDateTime())
                 .endingDateTime(event.getEndingDateTime())
                 .image(event.getImage())
+                .usersNicknames(event.getUsers().stream().map(User::getUsername).collect(Collectors.toSet()))
                 .build();
     }
 
