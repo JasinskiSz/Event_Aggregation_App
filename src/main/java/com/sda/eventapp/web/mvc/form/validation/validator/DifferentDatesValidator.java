3da62ede8b6bfffadbf7a0852d91a4165ab89cf5
Adriane0291 <adrianbloch01@gmail.com>
2023-01-03 14:16:52 +0100

    - change "CreateEventForm" to "EventForm"

--- src/main/java/com/sda/eventapp/web/mvc/form/validation/validator/DifferentDatesValidator.java
+++ src/main/java/com/sda/eventapp/web/mvc/form/validation/validator/DifferentDatesValidator.java
@@ -1,18 +1,18 @@
 package com.sda.eventapp.web.mvc.form.validation.validator;
 
-import com.sda.eventapp.web.mvc.form.CreateEventForm;
+import com.sda.eventapp.web.mvc.form.EventForm;
 import com.sda.eventapp.web.mvc.form.validation.constraint.DifferentDates;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class DifferentDatesValidator implements ConstraintValidator<DifferentDates, CreateEventForm> {
+public class DifferentDatesValidator implements ConstraintValidator<DifferentDates, EventForm> {
 
     @Override
     public void initialize(DifferentDates constraint) {
     }
 
     @Override
-    public boolean isValid(CreateEventForm form, ConstraintValidatorContext context) {
+    public boolean isValid(EventForm form, ConstraintValidatorContext context) {
         return form.getStartingDateTime()
                 .isBefore(form.getEndingDateTime());
     }
