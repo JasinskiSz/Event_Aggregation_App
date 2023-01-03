6f8d59ee65e7000e40f4f5014845ab48bc57db1f
Adriane0291 <adrianbloch01@gmail.com>
2023-01-03 14:22:40 +0100

    - fixed "long" to "Long"

--- src/main/java/com/sda/eventapp/model/User.java
+++ src/main/java/com/sda/eventapp/model/User.java
@@ -20,7 +20,7 @@ import java.util.Set;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     private String username;
 
