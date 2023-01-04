e913504304cf3c6f47466fe8f075188f7fbebf9a
Rabioxxx <szg.jasinski@gmail.com>
2023-01-04 20:55:17 +0100

    Handled "/" in url to be redirected to "/home"
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/main/java/com/sda/eventapp/web/mvc/controller/HomepageController.java
+++ src/main/java/com/sda/eventapp/web/mvc/controller/HomepageController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping({"/home", "/"})
 @RequiredArgsConstructor
 public class HomepageController {
     private final EventService eventService;
