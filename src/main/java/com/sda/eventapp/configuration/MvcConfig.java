8ff8cfa2116fc4590948b09907b6433d45e86d98
Rabioxxx <szg.jasinski@gmail.com>
2023-01-04 20:55:36 +0100

    Only login is needed to not be in redirect loop
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/main/java/com/sda/eventapp/configuration/MvcConfig.java
+++ src/main/java/com/sda/eventapp/configuration/MvcConfig.java
@@ -8,9 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("index");
-        registry.addViewController("/").setViewName("index");
         registry.addViewController("/login").setViewName("login");
-        registry.addViewController("/register").setViewName("create-user");
     }
 }
