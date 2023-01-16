988fd78bff647def9908c08da2c7d7abcca1a2a1
Adriane0291 <adrianbloch01@gmail.com>
2023-01-16 20:52:47 +0100

    - created user who is admin when starting the program

--- src/main/java/com/sda/eventapp/EventAggregationAppApplication.java
+++ src/main/java/com/sda/eventapp/EventAggregationAppApplication.java
@@ -1,13 +1,32 @@
 package com.sda.eventapp;
 
+import com.sda.eventapp.model.User;
+import com.sda.eventapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class EventAggregationAppApplication {
+public class EventAggregationAppApplication implements CommandLineRunner {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(EventAggregationAppApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        User user = new User();
+        user.setId(1);
+        user.setEmail("admin2@o2.pl");
+        user.setUsername("admin2@o2.pl");
+        user.setPassword(passwordEncoder.encode("haslo"));
+        user.setRole(User.Roles.ROLE_ADMIN);
+        repository.save(user);
+    }
 }
