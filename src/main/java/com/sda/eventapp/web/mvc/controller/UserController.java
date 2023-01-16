e752d5659e6e23bc18e2d3ff20e6ce7edb43a109
Przemek Degorski <degorskiprzemyslaw@gmail.com>
2023-01-16 16:27:31 +0100

    Fixed /user/register access - loggedUser is now redirected to /home

--- src/main/java/com/sda/eventapp/web/mvc/controller/UserController.java
+++ src/main/java/com/sda/eventapp/web/mvc/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.sda.eventapp.web.mvc.controller;
 
+import com.sda.eventapp.authentication.IAuthenticationFacade;
+import com.sda.eventapp.model.User;
 import com.sda.eventapp.service.UserService;
 import com.sda.eventapp.web.mvc.form.CreateUserForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
@@ -19,9 +23,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    private final IAuthenticationFacade authenticationFacade;
 
     @GetMapping("/register")
     public String register(ModelMap map) {
+        Authentication authentication = authenticationFacade.getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            return "redirect:/home";
+        }
         map.addAttribute("user", new CreateUserForm());
         return "create-user";
     }
