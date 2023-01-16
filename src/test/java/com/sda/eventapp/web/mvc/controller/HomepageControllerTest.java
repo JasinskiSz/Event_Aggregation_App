a4921795111a8ca3430538227ccb47d81754cfc3
Przemek Degorski <degorskiprzemyslaw@gmail.com>
2023-01-16 12:53:29 +0100

    Methods refactoring

--- src/test/java/com/sda/eventapp/web/mvc/controller/HomepageControllerTest.java
+++ src/test/java/com/sda/eventapp/web/mvc/controller/HomepageControllerTest.java
@@ -1,8 +1,6 @@
 package com.sda.eventapp.web.mvc.controller;
 
 import com.sda.eventapp.configuration.SecurityConfig;
-import com.sda.eventapp.model.User;
-import com.sda.eventapp.repository.UserRepository;
 import com.sda.eventapp.service.EventService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,57 +9,44 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Import(SecurityConfig.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource("/application-test.properties")
 //@WebMvcTest(HomepageController.class)
 class HomepageControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
     @Autowired
-    UserRepository userRepository;
+    private HomepageController homepageController;
+
+//    @MockBean
+//    private EventService eventService;
 
 
     @Test
     void shouldAllowAccessForAnonymousUser() throws Exception {
+
+        //todo null title handling
         mockMvc
                 .perform(MockMvcRequestBuilders.get("/home").param("title", ""))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
+
                 .andExpect(model().attributeExists("events"))
                 .andExpect(model().attributeExists("title"));
+
+
     }
 
-    private static User prepareTestData() {
-        return User.builder()
-                .username("user-test")
-                .email("user-test@gmail.com")
-                .password("useruser")
-                .build();
-    }
 
     @Test
-    void shouldAllowAccessForAuthenticatedUser() throws Exception {
-
-        User testUser1 = prepareTestData();
-        userRepository.save(testUser1);
-        mockMvc
-                .perform(MockMvcRequestBuilders.get("/home").param("title", "").with(user(userRepository.findById(testUser1.getId()).get())))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"))
-                .andExpect(model().attributeExists("events"))
-                .andExpect(model().attributeExists("title"));
-
-        userRepository.deleteAll();
+    void getAllEventsView() {
     }
 }
