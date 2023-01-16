7142e0bd2069c89dcc483dd1d6a98ae921cfd6ae
Rabioxxx <szg.jasinski@gmail.com>
2023-01-16 02:30:02 +0100

    Mapper for created previously DTOs
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/main/java/com/sda/eventapp/mapper/EventMapper.java
+++ src/main/java/com/sda/eventapp/mapper/EventMapper.java
@@ -1,12 +1,15 @@
 package com.sda.eventapp.mapper;
 
+import com.sda.eventapp.dto.EventApi;
 import com.sda.eventapp.dto.EventView;
 import com.sda.eventapp.model.Event;
 import com.sda.eventapp.model.User;
 import com.sda.eventapp.web.mvc.form.EventForm;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
@@ -59,5 +62,30 @@ public class EventMapper {
     public EventView[] toEventViewArray(List<Event> events) {
         List<EventView> eventViews = this.toEventViewList(events);
         return eventViews.toArray(new EventView[0]);
+    }
+
+    public List<EventApi> toEventApiList(List<Event> events) {
+        return events.stream()
+                .map(event -> EventApi.builder()
+                        .id(event.getId())
+                        .title(event.getTitle())
+                        .description(event.getDescription())
+                        .startingDateTime(event.getStartingDateTime())
+                        .endingDateTime(event.getEndingDateTime())
+                        .image(event.getImage())
+                        .attenders(getUsersNicknames(event))
+                        .ownerNickname(getOwnerNickname(event))
+                        .build())
+                .toList();
+    }
+
+    private Set<String> getUsersNicknames(@NotNull Event event) {
+        return event.getUsers().stream()
+                .map(User::getUsername)
+                .collect(Collectors.toSet());
+    }
+
+    private String getOwnerNickname(@NotNull Event event) {
+        return event.getOwner().getUsername();
     }
 }
