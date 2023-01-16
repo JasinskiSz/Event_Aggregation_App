d56119042e13632ce0568f746ebf59ed1654d908
Przemek Degorski <degorskiprzemyslaw@gmail.com>
2023-01-16 15:02:57 +0100

    Changed backend logic for EventDetailControllerTest methods

--- src/main/java/com/sda/eventapp/web/mvc/controller/EventDetailController.java
+++ src/main/java/com/sda/eventapp/web/mvc/controller/EventDetailController.java
@@ -6,6 +6,7 @@ import com.sda.eventapp.service.EventService;
 import com.sda.eventapp.web.mvc.form.CreateCommentForm;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -13,7 +14,13 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 
 @Controller
 @RequestMapping("/detail-view")
@@ -29,12 +36,12 @@ public class EventDetailController {
 
         if (!(authentication instanceof AnonymousAuthenticationToken)) {
             User loggedUser = (User) authenticationFacade.getAuthentication().getPrincipal();
+            map.addAttribute("comment", new CreateCommentForm());
             map.addAttribute("loggedUser", loggedUser);
         }
 
 
         map.addAttribute("event", eventService.findEventViewById(eventId));
-        map.addAttribute("comment", new CreateCommentForm());
         map.addAttribute("comments", eventService.findCommentViewsByEventId(eventId));
 
         return "event-detail-view";
@@ -59,6 +66,15 @@ public class EventDetailController {
     public String signupForEvent(@PathVariable("id") Long eventId) {
         User loggedUser = (User) authenticationFacade.getAuthentication().getPrincipal();
 
+        if (eventService.findByIdFetchOwnerFetchUsers(eventId).getOwner().getUsername().equals(loggedUser.getUsername())) {
+            throw new ResponseStatusException(FORBIDDEN, "ACCESS DENIED");
+        }
+        if (eventService.findByIdFetchOwnerFetchUsers(eventId).getStartingDateTime().isBefore(LocalDateTime.now())) {
+            throw new ResponseStatusException(BAD_REQUEST, "ACCESS DENIED");
+        }
+        if (eventService.findByIdFetchOwnerFetchUsers(eventId).getUsers().contains(loggedUser)) {
+            throw new ResponseStatusException(BAD_REQUEST, "ACCESS DENIED");
+        }
         eventService.signUpForEvent(loggedUser, eventId);
 
         return "redirect:/detail-view/" + eventId;
@@ -67,6 +83,16 @@ public class EventDetailController {
     @PostMapping("/{id}/sign-out-from-event")
     public String signOutFromEvent(@PathVariable("id") Long eventId) {
         User loggedUser = (User) authenticationFacade.getAuthentication().getPrincipal();
+
+        if (eventService.findByIdFetchOwnerFetchUsers(eventId).getOwner().getUsername().equals(loggedUser.getUsername())) {
+            throw new ResponseStatusException(FORBIDDEN, "ACCESS DENIED");
+        }
+        if (eventService.findByIdFetchOwnerFetchUsers(eventId).getStartingDateTime().isBefore(LocalDateTime.now())) {
+            throw new ResponseStatusException(BAD_REQUEST, "CANNOT SIGN OUT FROM EVENT HAS ALREADY STARTED");
+        }
+        if (!eventService.findByIdFetchOwnerFetchUsers(eventId).getUsers().contains(loggedUser)) {
+            throw new ResponseStatusException(BAD_REQUEST, "ACESS DENIED");
+        }
 
         eventService.signOutFromEvent(loggedUser, eventId);
 
