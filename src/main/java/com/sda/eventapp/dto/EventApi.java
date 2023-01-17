82a512a77167a1127a54c8afe9697adedaa44a86
Rabioxxx <szg.jasinski@gmail.com>
2023-01-17 14:54:17 +0100

    Fixed for serialization
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/main/java/com/sda/eventapp/dto/EventApi.java
+++ src/main/java/com/sda/eventapp/dto/EventApi.java
@@ -1,11 +1,15 @@
 package com.sda.eventapp.dto;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.Set;
 
 @Builder
+@Getter
+@Setter
 public class EventApi {
     private Long id;
     private String title;
--- src/main/java/com/sda/eventapp/dto/EventApiWrapper.java
+++ src/main/java/com/sda/eventapp/dto/EventApiWrapper.java
@@ -1,9 +1,13 @@
 package com.sda.eventapp.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @AllArgsConstructor
 public class EventApiWrapper {
     private List<EventApi> eventApiList;
