ff41bdfe6e914b464afbd0412f1a37ba51d436d5
Rabioxxx <szg.jasinski@gmail.com>
2023-01-22 03:02:55 +0100

    Changed method name
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/main/java/com/sda/eventapp/repository/CommentRepository.java
+++ src/main/java/com/sda/eventapp/repository/CommentRepository.java
@@ -1,10 +1,10 @@
 package com.sda.eventapp.repository;
 
 import com.sda.eventapp.model.Comment;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CommentRepository extends CrudRepository<Comment, Long> {
-    List<Comment> findAllByEvent_IdOrderByWritingDateDesc(long event_id);
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findAllByEventIdOrderByWritingDateDesc(long eventId);
 }
