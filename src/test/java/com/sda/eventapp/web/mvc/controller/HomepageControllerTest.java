4df5e4d6a07c46667cf657a559dbef3d626e75cc
Rabioxxx <szg.jasinski@gmail.com>
2023-01-22 02:09:24 +0100

    Reworked HomepageControllerTest class and mocked service
    Signed-off-by: Rabioxxx <szg.jasinski@gmail.com>

--- src/test/java/com/sda/eventapp/web/mvc/controller/HomepageControllerTest.java
+++ src/test/java/com/sda/eventapp/web/mvc/controller/HomepageControllerTest.java
@@ -2,18 +2,20 @@ package com.sda.eventapp.web.mvc.controller;
 
 import com.sda.eventapp.configuration.SecurityConfig;
 import com.sda.eventapp.model.User;
-import com.sda.eventapp.repository.UserRepository;
-import org.junit.jupiter.api.AfterEach;
+import com.sda.eventapp.service.EventService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -24,55 +26,42 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @TestPropertySource("/application-test.properties")
 class HomepageControllerTest {
 
-    private static final String EXCEPTION_MESSAGE = "User not found";
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
-    private UserRepository userRepository;
 
+    private final static String TITLE = "testTitle";
+    @MockBean
+    private EventService eventService;
+
+    @BeforeEach
+    void prepareTests() {
+        Mockito.when(eventService.findAllEventViews(TITLE, false, false, false)).thenReturn(List.of());
+    }
 
     @Test
     void shouldAllowAccessForAnonymousUser() throws Exception {
-        mockMvc
-                .perform(MockMvcRequestBuilders
-                        .get("/home")
-                        .param("title", ""))
+        mockMvc.perform(MockMvcRequestBuilders.get("/home")
+                        .param("title", TITLE))
                 .andExpect(status().isOk())
                 .andExpect(view().name("index"))
                 .andExpect(model().attributeExists("events"))
                 .andExpect(model().attributeExists("title"));
     }
 
-    @Nested
-    class HomepageControllerTestWithUserTestData {
-        private User testUser;
+    @Test
+    void shouldAllowAccessForAuthenticatedUser() throws Exception {
+        User testUser = User.builder()
+                .username("user-test")
+                .email("user-test@gmail.com")
+                .password("usertest")
+                .build();
 
-        @BeforeEach
-        void prepareTestData() {
-            testUser = User.builder()
-                    .username("user-test")
-                    .email("user-test@gmail.com")
-                    .password("usertest")
-                    .build();
-            userRepository.save(testUser);
-        }
-
-        @AfterEach
-        void deleteDataFromDatabase() {
-            userRepository.deleteAll();
-        }
-
-        @Test
-        void shouldAllowAccessForAuthenticatedUser() throws Exception {
-            mockMvc
-                    .perform(MockMvcRequestBuilders.get("/home")
-                            .param("title", "")
-                            .with(user(userRepository.findById(testUser.getId())
-                                    .orElseThrow(() -> new RuntimeException(EXCEPTION_MESSAGE)))))
-                    .andExpect(status().isOk())
-                    .andExpect(view().name("index"))
-                    .andExpect(model().attributeExists("events"))
-                    .andExpect(model().attributeExists("title"));
-        }
+        mockMvc.perform(MockMvcRequestBuilders.get("/home")
+                        .param("title", TITLE)
+                        .with(user(testUser)))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))
+                .andExpect(model().attributeExists("events"))
+                .andExpect(model().attributeExists("title"));
     }
 }
